854c7be0e9c29a3454c33c89840f0bf117d710d4
manishkuma123 <manishp.dotpitchtechnologies@gmail.com>
2026-05-25 11:33:20 +0530

    code updated

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  let db ='mongodb+srv://manishpdotpitchtechnologies_db_user:A6LqD3huz2DBa7d8@cluster0.wnz83zy.mongodb.net/wheeler?retryWrites=true&w=majority'
+  // let db ='mongodb+srv://manishpdotpitchtechnologies_db_user:A6LqD3huz2DBa7d8@cluster0.wnz83zy.mongodb.net/wheeler?retryWrites=true&w=majority'
+   const newDB =
+  "mongodb+srv://dotpitch123_db_user:i5hvfhVdDcZ1SmDH@cluster0.twzi3i1.mongodb.net/wheeler?retryWrites=true&w=majority";
+
   try {
-    const conn = await mongoose.connect(db);
+    const conn = await mongoose.connect(newDB);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
